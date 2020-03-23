70119cf40a98131f85abfc3e456191984010d0ca
brunomiguel174 <brunofernandes174@hotmail.com>
2020-03-23 12:16:30 +0000

    Fetch

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import cx from "classnames";
-
 import "./styles.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 100 };
+    this.state = { count: 100, error : "" };
     this.liked = false;
   }
 
   like = () => {
-    if (!this.liked) {
-      this.liked = true;
-      this.setState(({ count }) => ({ count: count + 1 }));
-      return;
-    }
-    this.liked = false;
-    this.setState(({ count }) => ({ count: count - 1 }));
+    fetch("http://localhost:3000/")
+      .then(req => req.json())
+      .then(data => {
+        console.log(data);
+        if (data) {
+          this.setState({error : ""});
+          if (!this.liked) {
+            this.liked = true;
+            this.setState(({ count }) => ({ count: count + 1 }));
+            return;
+          }
+          this.liked = false;
+          this.setState(({ count }) => ({ count: count - 1 }));
+        } else  {
+          this.setState({error : "a big error has occurred"})
+        }
+      });
   };
+
+  //   if (!this.liked) {
+  //     this.liked = true;
+  //     this.setState(({ count }) => ({ count: count + 1 }));
+  //     return;
+  //   }
+  //   this.liked = false;
+  //   this.setState(({ count }) => ({ count: count - 1 }));
+  // };
 
   render() {
     const { count } = this.state;
     return (
+      <>
       <button
         type="button"
         onClick={this.like}
@@ -31,6 +50,10 @@ class App extends Component {
       >
         Like | <span className="likes-counter">{count}</span>
       </button>
+      <br></br>
+      <br></br>
+      {this.state.error}
+      </>
     );
   }
 }
